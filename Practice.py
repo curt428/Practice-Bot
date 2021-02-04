24242f1e7b3be6b33aabd30aaad87111f56d6219
Curtis Healy <curtis.healy@signify.com>
2021-02-03 21:31:07 -0500

    made a guessing game

--- Practice.py
+++ Practice.py
@@ -1,1 +1,18 @@
-print("Close Rings")
+import random
+
+print("Put your guess here")
+try:
+    userInput = int(input())
+except:
+    userInput = 4
+    print("idiot alert")
+
+
+computerNumber = random.randint(1, 5)
+print(f'Here is your guess: {userInput}')
+
+if userInput == computerNumber:
+    print("Wow you are actually right for once")
+else: 
+    print(f'You are really wrong dumbass, this is what it was: {computerNumber}')
+
